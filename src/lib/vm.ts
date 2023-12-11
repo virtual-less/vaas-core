1c4f2761aa56496ac64686e00c843740783720e4
zy445566 <zyct@vip.qq.com>
2023-12-11 17:06:14 +0800

    feat: 添加eslint

--- src/lib/vm.ts
+++ src/lib/vm.ts
@@ -1,200 +1,203 @@
-import { readFileSync } from 'fs';
-import { createRequire } from 'module';
-import * as  path from 'path';
-import * as  vm from 'vm';
-import * as  os from 'os';
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/ban-types */
+import { readFileSync } from 'fs'
+import { createRequire } from 'module'
+import * as path from 'path'
+import * as vm from 'vm'
+import * as os from 'os'
 
 export interface DynamicRunParamsType {
-    filepath:string,
-    vmTimeout?:number,
-    extendVer?:NodeJS.Dict<any>,
-    isGlobalContext?:boolean,
-    overwriteRequire?:OverwriteRequire
-    overwriteReadCodeSync?:OverwriteReadCodeSync
+  filepath: string
+  vmTimeout?: number
+  extendVer?: NodeJS.Dict<any>
+  isGlobalContext?: boolean
+  overwriteRequire?: OverwriteRequire
+  overwriteReadCodeSync?: OverwriteReadCodeSync
 }
 
-export interface OverwriteRequire{
-    (callbackData:{
-        nativeRequire:NodeRequire,
-        filepath:string,
-        moduleId:string,
-        modulePath:string
-    }): any
-}
-export interface OverwriteReadCodeSync{
-    (filepath:string): string
-}
-interface InnerRunParamsType extends DynamicRunParamsType  {
-    context:vm.Context,
-    vmTimeout:number,
-    extendVer:NodeJS.Dict<any>,
-    overwriteRequire:OverwriteRequire
-    overwriteReadCodeSync:OverwriteReadCodeSync
+export type OverwriteRequire = (callbackData: {
+  nativeRequire: NodeRequire
+  filepath: string
+  moduleId: string
+  modulePath: string
+}) => any
+export type OverwriteReadCodeSync = (filepath: string) => string
+interface InnerRunParamsType extends DynamicRunParamsType {
+  context: vm.Context
+  vmTimeout: number
+  extendVer: NodeJS.Dict<any>
+  overwriteRequire: OverwriteRequire
+  overwriteReadCodeSync: OverwriteReadCodeSync
 }
 
-function genModuleRequire({
-    filepath, vmTimeout, 
-    context, extendVer, isGlobalContext,
-    overwriteRequire, overwriteReadCodeSync
-}:InnerRunParamsType):Function {
-    return (moduleId) => {
-        const nativeRequire:NodeRequire = createRequire(filepath);
-        const modulePath = nativeRequire.resolve(moduleId);
-        const module = overwriteRequire({
-            nativeRequire,
-            filepath,
-            moduleId,
-            modulePath
-        })
-        if(module) {return module}
-        return innerRun({
-            vmTimeout,
-            filepath:modulePath, 
-            context,
-            extendVer,
-            isGlobalContext,
-            overwriteRequire,
-            overwriteReadCodeSync
-        })
+function genModuleRequire ({
+  filepath, vmTimeout,
+  context, extendVer, isGlobalContext,
+  overwriteRequire, overwriteReadCodeSync
+}: InnerRunParamsType): Function {
+  return (moduleId) => {
+    const nativeRequire: NodeRequire = createRequire(filepath)
+    const modulePath = nativeRequire.resolve(moduleId)
+    const module = overwriteRequire({
+      nativeRequire,
+      filepath,
+      moduleId,
+      modulePath
+    })
+    if (module) { return module }
+    return innerRun({
+      vmTimeout,
+      filepath: modulePath,
+      context,
+      extendVer,
+      isGlobalContext,
+      overwriteRequire,
+      overwriteReadCodeSync
+    })
+  }
+}
+
+export function proxyData<T extends Object> (data: T): T {
+  if (
+    !(data instanceof Object)
+  ) {
+    return data
+  }
+  const proxyValue = {}
+  return new Proxy<T>(data, {
+    get (target, propKey, receiver) {
+      if (!proxyValue[propKey]) {
+        proxyValue[propKey] = proxyData(Reflect.get(target, propKey, receiver))
+      }
+      const propertyDescriptor = Reflect.getOwnPropertyDescriptor(target, propKey)
+      if (propertyDescriptor && !propertyDescriptor.configurable) {
+        return proxyValue[propKey]
+      }
+      return proxyValue[propKey]
+    },
+    getPrototypeOf (target) {
+      return proxyData(Reflect.getPrototypeOf(target))
+    },
+    getOwnPropertyDescriptor (target, propKey) {
+      if (!proxyValue[propKey]) {
+        Reflect.getOwnPropertyDescriptor(target, propKey)
+      }
+      return Reflect.getOwnPropertyDescriptor(proxyValue, propKey)
+    },
+    set (_target, propKey, value, receiver) {
+      return Reflect.set(proxyValue, propKey, value, receiver)
+    },
+    setPrototypeOf (_target, _value) {
+      return false
+    },
+    deleteProperty (_target, propKey) {
+      return Reflect.deleteProperty(proxyValue, propKey)
+    },
+    defineProperty (_target, propKey, propDesc) {
+      return Reflect.defineProperty(proxyValue, propKey, propDesc)
+    },
+    has (target, propKey) {
+      return Reflect.has(proxyValue, propKey) || Reflect.has(target, propKey)
+    },
+    ownKeys (target) {
+      return [...new Set([...Object.keys(target), ...Object.keys(proxyValue)]).values()]
     }
-}
-
-export function proxyData<T extends Object>(data:T):T {
-    if(
-       !(data instanceof Object)
-    ) {
-        return data;
-    }
-    let proxyValue = {}
-    return new Proxy<T>(data, {
-        get(target, propKey, receiver) {
-            if(!proxyValue[propKey]) {
-                proxyValue[propKey] = proxyData(Reflect.get(target, propKey, receiver))
-            }
-            const propertyDescriptor = Reflect.getOwnPropertyDescriptor(target, propKey)
-            if(propertyDescriptor && propertyDescriptor.configurable===false) {
-                return proxyValue[propKey]
-            }
-            return proxyValue[propKey];
-        },
-        getPrototypeOf(target) {
-            return proxyData(Reflect.getPrototypeOf(target));
-        },
-        getOwnPropertyDescriptor(target, propKey) {
-            if(!proxyValue[propKey]) {
-                Reflect.getOwnPropertyDescriptor(target, propKey)
-            }
-            return Reflect.getOwnPropertyDescriptor(proxyValue, propKey)
-        },
-        set(_target, propKey, value, receiver) {
-            return Reflect.set(proxyValue, propKey, value, receiver);
-        },
-        setPrototypeOf(_target, _value) {
-            return false
-        },
-        deleteProperty(_target, propKey) {
-            return Reflect.deleteProperty(proxyValue,propKey)
-        },
-        defineProperty(_target, propKey, propDesc) {
-            return Reflect.defineProperty(proxyValue, propKey, propDesc)
-        },
-        has(target, propKey) {
-            return Reflect.has(proxyValue, propKey) || Reflect.has(target, propKey)
-        },
-        ownKeys(target) {
-            return [...new Set([...Object.keys(target),...Object.keys(proxyValue)]).values()]
-        }
   })
 }
 
-function innerRun({
-    context,
+function innerRun ({
+  context,
+  filepath,
+  extendVer,
+  isGlobalContext,
+  vmTimeout,
+  overwriteRequire,
+  overwriteReadCodeSync
+}: InnerRunParamsType): any {
+  if (context.moduleCache[filepath]) {
+    return context.moduleCache[filepath]
+  }
+  const vmModule: {
+    exports: NodeJS.Dict<any>
+  } = { exports: {} }
+  // 为什么要设置moduleCache，原因是解决相互引用死循环问题,且导出变量为默认的exports值
+  context.moduleCache[filepath] = vmModule.exports
+  const vmRequire: Function = genModuleRequire({
     filepath,
+    vmTimeout,
+    context,
     extendVer,
     isGlobalContext,
+    overwriteRequire,
+    overwriteReadCodeSync
+  })
+
+  const moduleParams = {
+    require: vmRequire,
+    module: vmModule,
+    exports: vmModule.exports,
+    __filename: filepath,
+    __dirname: path.dirname(filepath),
+    console: proxyData(console),
+    ...extendVer
+  }
+  const moduleParamsKeys = Object.keys(moduleParams)
+  context.moduleData[filepath] = moduleParams
+  const vmOption: any = {
+    filename: filepath,
+    lineOffset: 1
+  }
+  if (vmTimeout > 0) {
+    vmOption.timeout = vmTimeout
+  }
+  const code = `moduleData['${filepath}'].moduleFunction = function (
+        ${moduleParamsKeys.join(',')}
+    ) {` + os.EOL +
+    overwriteReadCodeSync(filepath) +
+    os.EOL + `};
+    moduleData['${filepath}'].moduleFunction(
+        ${moduleParamsKeys.map(key => `moduleData['${filepath}'].${key}`).join(',')}
+    )`
+  if (context.isGlobalContext) {
+    vm.runInThisContext(code, vmOption)
+  } else {
+    vm.runInContext(code, context, vmOption)
+  }
+  // 这里是处理module.exports重新被赋值问题
+  context.moduleCache[filepath] = vmModule.exports
+  return context.moduleCache[filepath]
+}
+
+export function dynamicRun ({
+  filepath,
+  extendVer = {},
+  vmTimeout = 0,
+  isGlobalContext = false,
+  overwriteRequire = () => {},
+  overwriteReadCodeSync = (filepath) => { return readFileSync(filepath).toString() }
+}: DynamicRunParamsType): any {
+  if (!path.isAbsolute(filepath)) {
+    throw new Error('filepath must be absolute path')
+  }
+  const contextObj = {
+    moduleData: {},
+    moduleCache: {}
+  }
+  let context
+  if (isGlobalContext) {
+    context = vm.createContext(Object.assign(globalThis, contextObj))
+  } else {
+    context = vm.createContext(contextObj)
+  }
+
+  return innerRun({
+    context,
+    extendVer,
+    isGlobalContext,
+    filepath,
     vmTimeout,
     overwriteRequire,
     overwriteReadCodeSync
-}:InnerRunParamsType) {
-    if(context.moduleCache[filepath]) {
-        return context.moduleCache[filepath]
-    }
-    const vmModule:{
-        exports:NodeJS.Dict<any>
-    } = {exports:{}}
-    // 为什么要设置moduleCache，原因是解决相互引用死循环问题,且导出变量为默认的exports值
-    context.moduleCache[filepath] = vmModule.exports
-    const vmRequire:Function = genModuleRequire({
-        filepath, vmTimeout, 
-        context, extendVer, isGlobalContext, 
-        overwriteRequire,overwriteReadCodeSync
-    });
-    
-    const moduleParams = {
-        require:vmRequire,
-        module:vmModule,
-        exports:vmModule.exports,
-        __filename:filepath,
-        __dirname:path.dirname(filepath),
-        console:proxyData(console),
-        ...extendVer
-    }
-    const moduleParamsKeys = Object.keys(moduleParams)
-    context.moduleData[filepath] = moduleParams;
-    const vmOption:any = {
-        filename:filepath,
-        lineOffset:1,
-    }
-    if(vmTimeout>0) {
-        vmOption.timeout = vmTimeout
-    }
-    const code = `moduleData['${filepath}'].moduleFunction = function (
-        ${moduleParamsKeys.join(',')}
-    ) {`+os.EOL+
-    overwriteReadCodeSync(filepath)
-    +os.EOL+`};
-    moduleData['${filepath}'].moduleFunction(
-        ${moduleParamsKeys.map(key=>`moduleData['${filepath}'].${key}`).join(',')}
-    )`
-    if(context.isGlobalContext) {
-        vm.runInThisContext(code, vmOption)
-    } else {
-        vm.runInContext(code, context, vmOption)
-    }
-    // 这里是处理module.exports重新被赋值问题
-    context.moduleCache[filepath] =  vmModule.exports;
-    return context.moduleCache[filepath]
-}
-
-export function dynamicRun({
-    filepath,
-    extendVer={},
-    vmTimeout=0,
-    isGlobalContext=false,
-    overwriteRequire=()=>{},
-    overwriteReadCodeSync=(filepath)=>{return readFileSync(filepath).toString()}
-}:DynamicRunParamsType) {
-    if(!path.isAbsolute(filepath)) {
-        throw new Error('filepath must be absolute path')
-    }
-    const contextObj = {
-        moduleData:{},
-        moduleCache:{}
-    }
-    let context;
-    if(isGlobalContext) {
-        context = vm.createContext(Object.assign(globalThis,contextObj))
-    } else {
-        context = vm.createContext(contextObj)
-    }
-    
-    return innerRun({
-        context,
-        extendVer,
-        isGlobalContext,
-        filepath,
-        vmTimeout,
-        overwriteRequire,
-        overwriteReadCodeSync
-    })
+  })
 }
--- src/test/index.ts
+++ src/test/index.ts
@@ -1,209 +1,202 @@
 import * as assert from 'assert'
-import {describe, it} from 'mocha';
+import { describe, it } from 'mocha'
 import * as path from 'path'
 import * as fs from 'fs'
 import { dynamicRun, proxyData } from '../index'
 
-
 describe('test', function () {
-    it('test.run.add', async function() {
-        const exports = dynamicRun({
-            filepath:path.join(__dirname,'test.run.add.js'),
-            overwriteReadCodeSync:()=>{
-                return `
+  it('test.run.add', async function () {
+    const exports = dynamicRun({
+      filepath: path.join(__dirname, 'test.run.add.js'),
+      overwriteReadCodeSync: () => {
+        return `
                 let res = 1+1;
                 exports.res=res;
             `
-            }
-        })
-        assert.equal(
-            exports.res,
-            2,
-            'test.run.add error'
-        )
-    });
+      }
+    })
+    assert.equal(
+      exports.res,
+      2,
+      'test.run.add error'
+    )
+  })
 
-    it('test.scope.extendVer', async function() {
-        const hello = {}
-        const exports = dynamicRun({
-            filepath:path.join(__dirname,'test.scope.extendVer.js'),
-            overwriteReadCodeSync:()=>{
-                return `
+  it('test.scope.extendVer', async function () {
+    const hello = {}
+    const exports = dynamicRun({
+      filepath: path.join(__dirname, 'test.scope.extendVer.js'),
+      overwriteReadCodeSync: () => {
+        return `
                 hello.a = 1
                 exports.hello=hello;
             `
-            },
-            extendVer:{
-                hello
-            }
-        })
-        assert.equal(
-            exports.hello.a,
-            1,
-            'test.scope.extendVer error'
-        )
-    });
+      },
+      extendVer: {
+        hello
+      }
+    })
+    assert.equal(
+      exports.hello.a,
+      1,
+      'test.scope.extendVer error'
+    )
+  })
 
-    it('test.scope.extendVer.Array', async function() {
-        const exports = dynamicRun({
-            filepath:path.join(__dirname,'test.scope.extendVer.Array.js'),
-            overwriteReadCodeSync:()=>{
-                return `exports.res=Array;`
-            },
-            extendVer:{
-                Array:null
-            }
-        })
-        assert.equal(
-            exports.res,
-            null,
-            'test.scope.extendVer.Array error'
-        )
-    });
+  it('test.scope.extendVer.Array', async function () {
+    const exports = dynamicRun({
+      filepath: path.join(__dirname, 'test.scope.extendVer.Array.js'),
+      overwriteReadCodeSync: () => {
+        return 'exports.res=Array;'
+      },
+      extendVer: {
+        Array: null
+      }
+    })
+    assert.equal(
+      exports.res,
+      null,
+      'test.scope.extendVer.Array error'
+    )
+  })
 
-    it('test.scope.process', async function() {
-        dynamicRun({
-            filepath:path.join(__dirname,'test.scope.process.js'),
-            overwriteReadCodeSync:()=>{
-                return `process.env.a = 1`
-            },
-            extendVer:{
-                process
-            }
-        })    
-        assert.equal(
-            // @ts-ignore
-            process.env.a,
-            1,
-            'test.scope.process error'
-        )
-    });
+  it('test.scope.process', async function () {
+    dynamicRun({
+      filepath: path.join(__dirname, 'test.scope.process.js'),
+      overwriteReadCodeSync: () => {
+        return 'process.env.a = 1'
+      },
+      extendVer: {
+        process
+      }
+    })
+    assert.equal(
+      process.env.a,
+      1,
+      'test.scope.process error'
+    )
+  })
 
-    it('test.scope.process.proxyData', async function() {
-        dynamicRun({
-            filepath:path.join(__dirname,'test.scope.process.js'),
-            overwriteReadCodeSync:()=>{
-                return `process.env.b = 1`
-            },
-            extendVer:{
-                process:proxyData(process)
-            }
-        })    
-        assert.equal(
-            // @ts-ignore
-            process.env.b,
-            undefined,
-            'test.scope.process.proxyData error'
-        )
-    });
+  it('test.scope.process.proxyData', async function () {
+    dynamicRun({
+      filepath: path.join(__dirname, 'test.scope.process.js'),
+      overwriteReadCodeSync: () => {
+        return 'process.env.b = 1'
+      },
+      extendVer: {
+        process: proxyData(process)
+      }
+    })
+    assert.equal(
+      process.env.b,
+      undefined,
+      'test.scope.process.proxyData error'
+    )
+  })
 
-    it('test.scope.instanceof', async function() {
-        const extendVer:{[key:string]:any} = {}
-        const exports = dynamicRun({
-            filepath:path.join(__dirname,'test.scope.instanceof.js'),
-            overwriteReadCodeSync:(filepath)=>{
-                if(filepath===path.join(__dirname,'test.scope.instanceof.js')) {
-                    return `
+  it('test.scope.instanceof', async function () {
+    const extendVer: Record<string, any> = {}
+    const exports = dynamicRun({
+      filepath: path.join(__dirname, 'test.scope.instanceof.js'),
+      overwriteReadCodeSync: (filepath) => {
+        if (filepath === path.join(__dirname, 'test.scope.instanceof.js')) {
+          return `
                     const instanceofHello = require('./instanceof.js')
                     exports.res= instanceofHello.hello instanceof Array
                     `
-                }
-                return fs.readFileSync(filepath).toString()
-                
-            },
-            extendVer
-        })
-        assert.equal(
-            exports.res,
-            true,
-            'test.scope.instanceof error'
-        )
-    });
-
-    it('test.scope.Array', async function() {
-        dynamicRun({
-            filepath:path.join(__dirname,'test.scope.Array.js'),
-            overwriteReadCodeSync:()=>{
-                return `Array.aaa = 111`
-            },
-        })
-        assert.equal(
-            // @ts-ignore
-            Array.aaa,
-            undefined,
-            'test.scope.Array error'
-        )
-    });
-
-    it('test.require', async function() {
-        const exports = dynamicRun({
-            filepath:path.join(__dirname,'test.require.js'),
-            overwriteReadCodeSync:(filepath)=>{
-                if(filepath===path.join(__dirname,'test.require.js')) {
-                    return `exports.res = require('./a.js')`
-                }
-                return fs.readFileSync(filepath).toString()
-            },
-        })
-        assert.deepEqual(
-            exports.res,
-            {name:'a'},
-            'test.require error'
-        )
-    });
-
-    it('test.vmTimeout', async function() {
-        try {
-            dynamicRun({
-                filepath:path.join(__dirname,'test.vmTimeout.js'),
-                vmTimeout:3000,
-                overwriteReadCodeSync:()=>{
-                    return `while(true) {}`
-                },
-            })
-        } catch (error) {
-            assert.deepEqual(
-                error.message,
-                'Script execution timed out after 3000ms',
-                'test.vmTimeout error'
-            )
         }
-        
-    });
+        return fs.readFileSync(filepath).toString()
+      },
+      extendVer
+    })
+    assert.equal(
+      exports.res,
+      true,
+      'test.scope.instanceof error'
+    )
+  })
 
-    it('test.buffer', async function() {
-        const exports = dynamicRun({
-            filepath:path.join(__dirname,'test.buffer.js'),
-            vmTimeout:3000,
-            isGlobalContext: true,
-            overwriteReadCodeSync:()=>{
-                return `exports.buf = Buffer.from([1]).toString()`
-            },
-        })
-        assert.equal(
-            exports.buf,
-            Buffer.from([1]).toString(),
-            'test.buffer error'
-        )
-    });
+  it('test.scope.Array', async function () {
+    dynamicRun({
+      filepath: path.join(__dirname, 'test.scope.Array.js'),
+      overwriteReadCodeSync: () => {
+        return 'Array.aaa = 111'
+      }
+    })
+    assert.equal(
+      // @ts-expect-error
+      Array.aaa,
+      undefined,
+      'test.scope.Array error'
+    )
+  })
 
-    it('test.overwriteRequire', async function() {
-        const exports = dynamicRun({
-            filepath:path.join(__dirname,'test.overwriteRequire.js'),
-            vmTimeout:3000,
-            overwriteRequire:(callbackData)=>{
-                return callbackData.modulePath
-            },
-            overwriteReadCodeSync:()=>{
-                return `exports.fs = require('fs')`
-            },
-        })
-        assert.equal(
-            exports.fs,
-            'fs',
-            'test.overwriteRequire error'
-        )
-    });
+  it('test.require', async function () {
+    const exports = dynamicRun({
+      filepath: path.join(__dirname, 'test.require.js'),
+      overwriteReadCodeSync: (filepath) => {
+        if (filepath === path.join(__dirname, 'test.require.js')) {
+          return 'exports.res = require(\'./a.js\')'
+        }
+        return fs.readFileSync(filepath).toString()
+      }
+    })
+    assert.deepEqual(
+      exports.res,
+      { name: 'a' },
+      'test.require error'
+    )
+  })
 
+  it('test.vmTimeout', async function () {
+    try {
+      dynamicRun({
+        filepath: path.join(__dirname, 'test.vmTimeout.js'),
+        vmTimeout: 3000,
+        overwriteReadCodeSync: () => {
+          return 'while(true) {}'
+        }
+      })
+    } catch (error) {
+      assert.deepEqual(
+        error.message,
+        'Script execution timed out after 3000ms',
+        'test.vmTimeout error'
+      )
+    }
+  })
+
+  it('test.buffer', async function () {
+    const exports = dynamicRun({
+      filepath: path.join(__dirname, 'test.buffer.js'),
+      vmTimeout: 3000,
+      isGlobalContext: true,
+      overwriteReadCodeSync: () => {
+        return 'exports.buf = Buffer.from([1]).toString()'
+      }
+    })
+    assert.equal(
+      exports.buf,
+      Buffer.from([1]).toString(),
+      'test.buffer error'
+    )
+  })
+
+  it('test.overwriteRequire', async function () {
+    const exports = dynamicRun({
+      filepath: path.join(__dirname, 'test.overwriteRequire.js'),
+      vmTimeout: 3000,
+      overwriteRequire: (callbackData) => {
+        return callbackData.modulePath
+      },
+      overwriteReadCodeSync: () => {
+        return 'exports.fs = require(\'fs\')'
+      }
+    })
+    assert.equal(
+      exports.fs,
+      'fs',
+      'test.overwriteRequire error'
+    )
+  })
 })
-
